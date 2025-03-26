7aa6426f300f2fcb0bfba57061937828ae1cc56a
Adam Zalewski <zalewskiadam220@gmail.com>
2025-03-26 21:11:52 +0100

    installer

--- CC/ae2/SnowflakesAE2Installer.lua
+++ CC/ae2/SnowflakesAE2Installer.lua
@@ -30,47 +30,47 @@ local base64 = {}
 
 local extract = _G.bit32 and _G.bit32.extract -- Lua 5.2/Lua 5.3 in compatibility mode
 if not extract then
-	if _G.bit then -- LuaJIT
+	if _G.bit then                            -- LuaJIT
 		local shl, shr, band = _G.bit.lshift, _G.bit.rshift, _G.bit.band
-		extract = function( v, from, width )
-			return band( shr( v, from ), shl( 1, width ) - 1 )
+		extract = function(v, from, width)
+			return band(shr(v, from), shl(1, width) - 1)
 		end
 	elseif _G._VERSION == "Lua 5.1" then
-		extract = function( v, from, width )
+		extract = function(v, from, width)
 			local w = 0
-			local flag = 2^from
-			for i = 0, width-1 do
+			local flag = 2 ^ from
+			for i = 0, width - 1 do
 				local flag2 = flag + flag
 				if v % flag2 >= flag then
-					w = w + 2^i
+					w = w + 2 ^ i
 				end
 				flag = flag2
 			end
 			return w
 		end
 	else -- Lua 5.3+
-		extract = load[[return function( v, from, width )
+		extract = load [[return function( v, from, width )
 			return ( v >> from ) & ((1 << width) - 1)
 		end]]()
 	end
 end
 
 
-function base64.makeencoder( s62, s63, spad )
+function base64.makeencoder(s62, s63, spad)
 	local encoder = {}
-	for b64code, char in pairs{[0]='A','B','C','D','E','F','G','H','I','J',
-		'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
-		'Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-		'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2',
-		'3','4','5','6','7','8','9',s62 or '+',s63 or'/',spad or'='} do
+	for b64code, char in pairs { [0] = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+		'3', '4', '5', '6', '7', '8', '9', s62 or '+', s63 or '/', spad or '=' } do
 		encoder[b64code] = char:byte()
 	end
 	return encoder
 end
 
-function base64.makedecoder( s62, s63, spad )
+function base64.makedecoder(s62, s63, spad)
 	local decoder = {}
-	for b64code, charcode in pairs( base64.makeencoder( s62, s63, spad )) do
+	for b64code, charcode in pairs(base64.makeencoder(s62, s63, spad)) do
 		decoder[charcode] = b64code
 	end
 	return decoder
@@ -81,83 +81,87 @@ local DEFAULT_DECODER = base64.makedecoder()
 
 local char, concat = string.char, table.concat
 
-function base64.encode( str, encoder, usecaching )
+function base64.encode(str, encoder, usecaching)
 	encoder = encoder or DEFAULT_ENCODER
 	local t, k, n = {}, 1, #str
 	local lastn = n % 3
 	local cache = {}
-	for i = 1, n-lastn, 3 do
-		local a, b, c = str:byte( i, i+2 )
-		local v = a*0x10000 + b*0x100 + c
+	for i = 1, n - lastn, 3 do
+		local a, b, c = str:byte(i, i + 2)
+		local v = a * 0x10000 + b * 0x100 + c
 		local s
 		if usecaching then
 			s = cache[v]
 			if not s then
-				s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
+				s = char(encoder[extract(v, 18, 6)], encoder[extract(v, 12, 6)], encoder[extract(v, 6, 6)],
+					encoder[extract(v, 0, 6)])
 				cache[v] = s
 			end
 		else
-			s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
+			s = char(encoder[extract(v, 18, 6)], encoder[extract(v, 12, 6)], encoder[extract(v, 6, 6)],
+				encoder[extract(v, 0, 6)])
 		end
 		t[k] = s
 		k = k + 1
 	end
 	if lastn == 2 then
-		local a, b = str:byte( n-1, n )
-		local v = a*0x10000 + b*0x100
-		t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[64])
+		local a, b = str:byte(n - 1, n)
+		local v = a * 0x10000 + b * 0x100
+		t[k] = char(encoder[extract(v, 18, 6)], encoder[extract(v, 12, 6)], encoder[extract(v, 6, 6)], encoder[64])
 	elseif lastn == 1 then
-		local v = str:byte( n )*0x10000
-		t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[64], encoder[64])
+		local v = str:byte(n) * 0x10000
+		t[k] = char(encoder[extract(v, 18, 6)], encoder[extract(v, 12, 6)], encoder[64], encoder[64])
 	end
-	return concat( t )
+	return concat(t)
 end
 
-function base64.decode( b64, decoder, usecaching )
+function base64.decode(b64, decoder, usecaching)
 	decoder = decoder or DEFAULT_DECODER
 	local pattern = '[^%w%+%/%=]'
 	if decoder then
 		local s62, s63
-		for charcode, b64code in pairs( decoder ) do
-			if b64code == 62 then s62 = charcode
-			elseif b64code == 63 then s63 = charcode
+		for charcode, b64code in pairs(decoder) do
+			if b64code == 62 then
+				s62 = charcode
+			elseif b64code == 63 then
+				s63 = charcode
 			end
 		end
-		pattern = ('[^%%w%%%s%%%s%%=]'):format( char(s62), char(s63) )
+		pattern = ('[^%%w%%%s%%%s%%=]'):format(char(s62), char(s63))
 	end
-	b64 = b64:gsub( pattern, '' )
+	b64 = b64:gsub(pattern, '')
 	local cache = usecaching and {}
 	local t, k = {}, 1
 	local n = #b64
 	local padding = b64:sub(-2) == '==' and 2 or b64:sub(-1) == '=' and 1 or 0
-	for i = 1, padding > 0 and n-4 or n, 4 do
-		local a, b, c, d = b64:byte( i, i+3 )
+	for i = 1, padding > 0 and n - 4 or n, 4 do
+		local a, b, c, d = b64:byte(i, i + 3)
 		local s
 		if usecaching then
-			local v0 = a*0x1000000 + b*0x10000 + c*0x100 + d
+			local v0 = a * 0x1000000 + b * 0x10000 + c * 0x100 + d
 			s = cache[v0]
 			if not s then
-				local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
-				s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
+				local v = decoder[a] * 0x40000 + decoder[b] * 0x1000 + decoder[c] * 0x40 + decoder[d]
+				s = char(extract(v, 16, 8), extract(v, 8, 8), extract(v, 0, 8))
 				cache[v0] = s
 			end
 		else
-			local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
-			s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
+			local v = decoder[a] * 0x40000 + decoder[b] * 0x1000 + decoder[c] * 0x40 + decoder[d]
+			s = char(extract(v, 16, 8), extract(v, 8, 8), extract(v, 0, 8))
 		end
 		t[k] = s
 		k = k + 1
 	end
 	if padding == 1 then
-		local a, b, c = b64:byte( n-3, n-1 )
-		local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40
-		t[k] = char( extract(v,16,8), extract(v,8,8))
+		local a, b, c = b64:byte(n - 3, n - 1)
+		local v = decoder[a] * 0x40000 + decoder[b] * 0x1000 + decoder[c] * 0x40
+		t[k] = char(extract(v, 16, 8), extract(v, 8, 8))
 	elseif padding == 2 then
-		local a, b = b64:byte( n-3, n-2 )
-		local v = decoder[a]*0x40000 + decoder[b]*0x1000
-		t[k] = char( extract(v,16,8))
+		local a, b = b64:byte(n - 3, n - 2)
+		local v = decoder[a] * 0x40000 + decoder[b] * 0x1000
+		t[k] = char(extract(v, 16, 8))
 	end
-	return concat( t )
+	return concat(t)
 end
 
 return base64
@@ -212,85 +216,98 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 local path = fs.getDir(shell.getRunningProgram())
 local programName = fs.getName(shell.getRunningProgram())
 local github = {
-    cache = {}
+	cache = {}
 }
 
 function github.get_json_value(json_str, key)
-    -- Escape special characters in the key
-    key = key:gsub("([%[%]%(%)%.%%%+%-%*%?%^%$])", "%%%1")
-    -- Match the key and extract the value
-    local pattern = '"' .. key .. '"%s*:%s*"(.-)"'
-    return json_str:match(pattern)
+	-- Escape special characters in the key
+	key = key:gsub("([%[%]%(%)%.%%%+%-%*%?%^%$])", "%%%1")
+	-- Match the key and extract the value
+	local pattern = '"' .. key .. '"%s*:%s*"(.-)"'
+	return json_str:match(pattern)
 end
 
 function github.convert_url(url)
-    url = url
-        :gsub("https://github.com/", "https://api.github.com/repos/")
-        :gsub("https://raw.githubusercontent.com/", "https://api.github.com/repos/")
-        :gsub("blob/.-/", "contents/")
-        :gsub("refs/heads/.-/", "contents/")
-    return url
+	url = url
+		:gsub("https://github.com/", "https://api.github.com/repos/")
+		:gsub("https://raw.githubusercontent.com/", "https://api.github.com/repos/")
+		:gsub("blob/.-/", "contents/")
+		:gsub("refs/heads/.-/", "contents/")
+	return url
 end
 
 function github:api_response(url)
-    -- Check if the URL is valid
-    local ok, err = http.checkURL(url)
-    if not ok then
-        printError(err or "Invalid URL.")
-        return
-    end
+	-- Check if the URL is valid
+	local ok, err = http.checkURL(url)
+	if not ok then
+		printError(err or "Invalid URL.")
+		return
+	end
 
-    local apiurl = self.convert_url(url)
+	local apiurl = self.convert_url(url)
 
-    if self.cache[apiurl] then
-        print("Returned cached")
-        return self.cache[apiurl]
-    end
+	if self.cache[apiurl] then
+		print("Returned cached")
+		return self.cache[apiurl]
+	end
 
-    local response = http.get(apiurl).readAll()
-    local content = self.get_json_value(response, "content"):gsub("\\n", "\n")
-    local name = self.get_json_value(response, "name"):gsub(" ", "_")
-    local data = base64.decode(content)
-    local output = {
-        name = name,
-        content = data,
-        response = response
-    }
-    github.cache[apiurl] = output
-    return output
+	local response = http.get(apiurl).readAll()
+	local content = self.get_json_value(response, "content"):gsub("\\n", "\n")
+	local name = self.get_json_value(response, "name"):gsub(" ", "_")
+	local data = base64.decode(content)
+	local output = {
+		name = name,
+		content = data,
+		response = response
+	}
+	github.cache[apiurl] = output
+	return output
 end
 
-function github.api(url)
-    
-    local response = github.api_response(url)
-    if response ~= nil then
-        local file = fs.open(path..(response.name), "w")
-        file.write(response.content)
-        file.close()
-    end
+function github.api(url, name)
+	local response = github.api_response(url)
+	if response ~= nil then
+		local file = fs.open(path .. (response.name), "w")
+		file.write(response.content)
+		file.close()
+		print("Downloaded", name)
+	else
+		printError("Download failed for", name)
+	end
 end
 
 -- ============================================================================
 -- ============================      INSTALLER       ==========================
 -- ============================================================================
 
-local githubBasePath = "https://github.com/akneroth/Hexcasting/tree/master/CC/ae2/new/"
-local files = {
-    base = {
-        githubBasePath.."libs/base.lua",
-        githubBasePath.."libs/ae2base.lua",
-    },
-    manager = {
-        githubBasePath.."ae2manager.lua",
-        githubBasePath.."ae2crafting.lua",
-    },
-    monitor = {
-        githubBasePath.."libs/ccui.lua",
-        githubBasePath.."ae2monitor.lua",
-    },
+local installer = {
+	githubBasePath = "https://github.com/akneroth/Hexcasting/tree/master/CC/ae2/new/",
+	files = {
+		base = {
+			"libs/base.lua",
+			"libs/ae2base.lua",
+		},
+		manager = {
+			"ae2manager.lua",
+			"ae2crafting.lua",
+		},
+		monitor = {
+			"libs/ccui.lua",
+			"ae2monitor.lua",
+		},
+	},
 }
 
-for _, v in ipairs(files.base) do github.api(v) end
-if arg[1] == "manager" then for _, v in ipairs(files.manager) do github.api(v) end end
-if arg[1] == "monitor" then for _, v in ipairs(files.monitor) do github.api(v) end end
-if arg[1] == "" or arg[1] == nil then print("Usage: ", programName, "manager|monitor") end
+
+function installer:install()
+	if arg[1] == "" or arg[1] == nil then
+		print("Usage: ", programName, "manager|monitor")
+		return self
+	else
+		for _, v in ipairs(self.files.base) do github.api(self.githubBasePath..v, v) end
+	end
+	if arg[1] == "manager" then for _, v in ipairs(self.files.manager) do github.api(self.githubBasePath..v, v) end end
+	if arg[1] == "monitor" then for _, v in ipairs(self.files.monitor) do github.api(self.githubBasePath..v, v) end end
+end
+
+installer:install()
